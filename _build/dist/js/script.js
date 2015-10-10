b2bfec63db328d6f6ad39294940199a13be7001d
Tajima Shingo <tajima-shingo@kayac.com>
2015-10-10 14:17:04 +0900

    html to canvas実装

--- _build/dist/js/script.js
+++ _build/dist/js/script.js
@@ -18,11 +18,51 @@ Main = (function() {
     this.exec();
   }
 
+  Main.prototype.htmlToCanvas = function($dom) {
+    var _$dom, _DOMURL, _canvas, _cssAttr, _cssTxt, _ctx, _data, _img, _svg, _url, j, k, ref;
+    _$dom = $dom.clone();
+    _cssAttr = ["font-family", "font-size", "font-weight", "font-style", "color", "text-transform", "text-decoration", "letter-spacing", "word-spacing", "line-height", "text-align", "vertical-align", "direction", "background-color", "background-image", "background-repeat", "background-position", "background-attachment", "opacity", "width", "height", "top", "right", "bottom", "left", "margin-top", "margin-right", "margin-bottom", "margin-left", "padding-top", "padding-right", "padding-bottom", "padding-left", "border-top-width", "border-right-width", "border-bottom-width", "border-left-width", "border-top-color", "border-right-color", "border-bottom-color", "border-left-color", "border-top-style", "border-right-style", "border-bottom-style", "border-left-style", "position", "display", "visibility", "z-index", "overflow-x", "overflow-y", "white-space", "clip", "float", "clear", "cursor", "list-style-image", "list-style-position", "list-style-type", "marker-offset"];
+    _cssTxt = "";
+    for (j = k = 0, ref = _cssAttr.length; 0 <= ref ? k < ref : k > ref; j = 0 <= ref ? ++k : --k) {
+      _cssTxt += _cssAttr[j] + ": " + ($.fn.css.call($dom, _cssAttr[j])) + "; ";
+    }
+    _$dom.attr({
+      style: _cssTxt
+    });
+    $dom.find("*").each(function(i) {
+      var l, ref1;
+      _cssTxt = "";
+      for (j = l = 0, ref1 = _cssAttr.length; 0 <= ref1 ? l < ref1 : l > ref1; j = 0 <= ref1 ? ++l : --l) {
+        _cssTxt += _cssAttr[j] + ": " + ($.fn.css.call($(this), _cssAttr[j])) + "; ";
+      }
+      return _$dom.find("*").eq(i).attr({
+        style: _cssTxt
+      });
+    });
+    _canvas = document.createElement("canvas");
+    _canvas.width = $dom.outerWidth(true);
+    _canvas.height = $dom.outerHeight(true);
+    _ctx = _canvas.getContext("2d");
+    _data = "<svg xmlns='http://www.w3.org/2000/svg' " + ("width='" + ($dom.outerWidth(true)) + "' ") + ("height='" + ($dom.outerHeight(true)) + "'>") + "<foreignObject width='100%' height='100%'>" + "<div xmlns='http://www.w3.org/1999/xhtml'>" + _$dom.get(0).outerHTML.replace(/data-(.*?)"\ |data-(.*?)">/g, "").replace(/<br(.*?)>/g, "<br$1\/>").replace(/<img(.*?)>/g, "<img$1\/>") + "</div>" + "</foreignObject>" + "</svg>";
+    _DOMURL = self.URL || self.webkitURL || self;
+    _img = new Image();
+    _svg = new Blob([_data], {
+      type: "image/svg+xml"
+    });
+    _url = _DOMURL.createObjectURL(_svg);
+    _img.onload = function() {
+      _ctx.drawImage(_img, 0, 0);
+      return _DOMURL.revokeObjectURL(_url);
+    };
+    _img.src = _url;
+    return _canvas;
+  };
+
   Main.prototype.setScrollBarHeight = function() {
-    var _t, _type, i, j, ref, results;
+    var _t, _type, i, k, ref, results;
     _type = ["t", "d"];
     results = [];
-    for (i = j = 0, ref = _type.length; 0 <= ref ? j < ref : j > ref; i = 0 <= ref ? ++j : --j) {
+    for (i = k = 0, ref = _type.length; 0 <= ref ? k < ref : k > ref; i = 0 <= ref ? ++k : --k) {
       _t = _type[i];
       results.push((function(_this) {
         return function(_t) {
@@ -35,7 +75,7 @@ Main = (function() {
   };
 
   Main.prototype.exec = function() {
-    var _t, _type, fn, i, j, ref;
+    var _loaded_count, _t, _type, fn, i, k, l, ref, ref1, results;
     this.$win.on("resize", $.debounce(500, (function(_this) {
       return function() {
         return _this.setScrollBarHeight();
@@ -77,11 +117,43 @@ Main = (function() {
         }));
       };
     })(this);
-    for (i = j = 0, ref = _type.length; 0 <= ref ? j < ref : j > ref; i = 0 <= ref ? ++j : --j) {
+    for (i = k = 0, ref = _type.length; 0 <= ref ? k < ref : k > ref; i = 0 <= ref ? ++k : --k) {
       _t = _type[i];
       fn(_t);
     }
-    return this.setScrollBarHeight();
+    this.setScrollBarHeight();
+    _loaded_count = 0;
+    results = [];
+    for (i = l = 0, ref1 = this.$thumb.size(); 0 <= ref1 ? l < ref1 : l > ref1; i = 0 <= ref1 ? ++l : --l) {
+      results.push((function(_this) {
+        return function(i) {
+          var _img, _interval;
+          _img = new Image();
+          _interval = setInterval(function() {
+            var _canvas, _ctx;
+            if (_img.width > 0) {
+              _canvas = document.createElement("canvas");
+              _ctx = _canvas.getContext("2d");
+              _canvas.width = _img.width;
+              _canvas.height = _img.height;
+              _ctx.drawImage(_img, 0, 0);
+              _this.$thumb.eq(i).find(".thumb_pic").css({
+                width: _img.width,
+                height: _img.height,
+                backgroundImage: "url(" + (_canvas.toDataURL()) + ")"
+              });
+              clearInterval(_interval);
+              _loaded_count += 1;
+              if (_loaded_count === _this.$thumb.size()) {
+                return $("body").append(_this.htmlToCanvas($(".thumb_container")));
+              }
+            }
+          }, 100);
+          return _img.src = "img/" + _this.$thumb.eq(i).find(".thumb_pic").attr("data-type") + "-thumb/" + _this.$thumb.eq(i).find(".thumb_pic").attr("data-name") + ".jpg";
+        };
+      })(this)(i));
+    }
+    return results;
   };
 
   return Main;
--- _build/src/coffee/pc/script.coffee
+++ _build/src/coffee/pc/script.coffee
@@ -20,6 +20,73 @@ class Main
 
         @exec()
 
+    htmlToCanvas: ($dom) ->
+        _$dom = $dom.clone()
+
+        _cssAttr = [ "font-family", "font-size", "font-weight", "font-style",
+                     "color",  "text-transform", "text-decoration",
+                     "letter-spacing", "word-spacing", "line-height",
+                     "text-align", "vertical-align", "direction",
+                     "background-color", "background-image",
+                     "background-repeat","background-position",
+                     "background-attachment", "opacity",
+                     "width", "height", "top", "right", "bottom",
+                     "left", "margin-top", "margin-right", "margin-bottom",
+                     "margin-left", "padding-top", "padding-right",
+                     "padding-bottom", "padding-left",
+                     "border-top-width", "border-right-width",
+                     "border-bottom-width","border-left-width",
+                     "border-top-color", "border-right-color",
+                     "border-bottom-color", "border-left-color",
+                     "border-top-style","border-right-style",
+                     "border-bottom-style", "border-left-style",
+                     "position", "display", "visibility",
+                     "z-index", "overflow-x","overflow-y", "white-space",
+                     "clip", "float", "clear", "cursor",
+                     "list-style-image", "list-style-position",
+                     "list-style-type", "marker-offset" ]
+
+        _cssTxt = ""
+        for j in [0..._cssAttr.length]
+            _cssTxt += "#{_cssAttr[j]}: #{$.fn.css.call($dom, _cssAttr[j])}; "
+        _$dom.attr style: _cssTxt
+
+        $dom.find("*").each (i) ->
+            _cssTxt = ""
+            for j in [0..._cssAttr.length]
+                _cssTxt +=
+                    "#{_cssAttr[j]}: #{$.fn.css.call($(this), _cssAttr[j])}; "
+            _$dom.find("*").eq(i).attr style: _cssTxt
+
+        _canvas = document.createElement "canvas"
+        _canvas.width = $dom.outerWidth true
+        _canvas.height = $dom.outerHeight true
+        _ctx = _canvas.getContext "2d"
+        _data = "<svg xmlns='http://www.w3.org/2000/svg' " +
+                "width='#{$dom.outerWidth true}' " +
+                "height='#{$dom.outerHeight true}'>" +
+                    "<foreignObject width='100%' height='100%'>" +
+                        "<div xmlns='http://www.w3.org/1999/xhtml'>" +
+                            _$dom.get(0).outerHTML.
+                            replace(/data-(.*?)"\ |data-(.*?)">/g, "").
+                            replace(/<br(.*?)>/g, "<br$1\/>").
+                            replace(/<img(.*?)>/g, "<img$1\/>") +
+                        "</div>" +
+                    "</foreignObject>" +
+                "</svg>"
+
+        _DOMURL = self.URL || self.webkitURL || self
+        _img = new Image()
+        _svg = new Blob([_data], type: "image/svg+xml")
+        _url = _DOMURL.createObjectURL _svg
+
+        _img.onload = ->
+            _ctx.drawImage _img, 0, 0
+            _DOMURL.revokeObjectURL _url
+        _img.src = _url
+
+        return _canvas
+
     setScrollBarHeight: ->
         _type = ["t", "d"]
 
@@ -85,5 +152,34 @@ class Main
         ######################
 
         @setScrollBarHeight()
+
+        _loaded_count = 0
+        for i in [0...@$thumb.size()]
+            do (i) =>
+                _img = new Image()
+                _interval = setInterval =>
+                    if _img.width > 0
+                        _canvas = document.createElement "canvas"
+                        _ctx = _canvas.getContext "2d"
+                        _canvas.width = _img.width
+                        _canvas.height = _img.height
+                        _ctx.drawImage _img, 0, 0
+                        @$thumb.eq(i).find(".thumb_pic").css
+                            width: _img.width
+                            height: _img.height
+                            backgroundImage: "url(#{_canvas.toDataURL()})"
+                        clearInterval _interval
+
+                        _loaded_count += 1
+                        if _loaded_count == @$thumb.size()
+                            $("body").append @htmlToCanvas $(".thumb_container")
+                , 100
+
+                _img.src =
+                    "img/" +
+                    @$thumb.eq(i).find(".thumb_pic").attr("data-type") +
+                    "-thumb/" +
+                    @$thumb.eq(i).find(".thumb_pic").attr("data-name") +
+                    ".jpg"
 
 new Main()
